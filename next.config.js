0c0bf2e59fa1aed121d6b2cb978872eb5639dd95
Damilola Israel Okewoye <damilola.okewoye@example.com>
2026-03-19 19:10:01 +0100

    Fix: complete static export config for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
-
 module.exports = nextConfig;
